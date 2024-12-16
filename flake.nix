8eb424f13073650eb3d8431c18dda38edf0cb310
Pavel Shirshov <pshirshov@eml.cc>
2024-12-16 15:46:37 +0000

    wip

--- flake.nix
+++ flake.nix
@@ -6,8 +6,10 @@
       systems = [
         "x86_64-linux"
         # "i686-linux"
-        # "x86_64-darwin"
-        # "aarch64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+
         # "armv6l-linux"
         # "armv7l-linux"
       ];
